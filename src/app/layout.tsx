af5c88453d6b70aad6241dd6cca1651363b3043a
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2025-07-28 00:34:17 +0900

    monkshark commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,32 +4,32 @@ import "./globals.css";
 import React from "react";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Policy Score",
-  description: "HansolHighSchool student council",
+    title: "Policy Score",
+    description: "HansolHighSchool student council",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+            {children}
+        </body>
+        </html>
+    );
 }
