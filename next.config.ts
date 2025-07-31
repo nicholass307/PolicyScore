b7e064866e38fdbf704ab8ac8bef6d905b2e8a0f
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2025-07-31 16:53:19 +0900

    monkshark commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+    output: "export",
+    basePath: isProd ? "/gps" : "",
+    assetPrefix: isProd ? "/gps/" : "",
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: '**',
+                protocol: "https",
+                hostname: "**",
             },
         ],
     },
