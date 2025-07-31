388f450471b90d25ac4fa61d48435c7b7a29837f
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2025-07-31 20:41:09 +0900

    monkshark commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -119,7 +119,7 @@ export default function Footer() {
 
                 <div className="mt-12 border-t border-gray-200 pt-8">
                     <p className="text-center text-sm text-gray-500">
-                        © 2024 제 13대 학생회  &quot;새솔&quot;.  All rights reserved.
+                        © 2024 제 13대 학생회  &#39;새솔&#39;.  All rights reserved.
                     </p>
                 </div>
             </div>
