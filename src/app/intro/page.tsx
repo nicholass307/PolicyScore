75944f63f0ccf733fd2496b05c064d82aa7fd14d
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2025-08-01 13:14:19 +0900

    monkshark commit

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -17,7 +17,6 @@ export default function IntroPage() {
                 </div>
             </div>
 
-            {/* Content Section */}
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16 lg:py-24">
                 <div className="max-w-4xl mx-auto">
                     <div className="space-y-12">
